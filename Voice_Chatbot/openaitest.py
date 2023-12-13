e5b2d4cc1d6a218837fee4ea9d7add40c0d1836a
Adish Jain <114593154+Adi93401@users.noreply.github.com>
2023-12-13 15:26:53 +0530

    Update openaitest.py

--- Voice_Chatbot/openaitest.py
+++ Voice_Chatbot/openaitest.py
@@ -5,14 +5,13 @@ from config import apikey
 openai.api_key = apikey
 
 response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Write an email to my boss for resignation?",
-  temperature=0.7,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
+    model="text-davinci-003",
+    prompt="Write an email to my boss for resignation?",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0)
 
 print(response)
 '''
