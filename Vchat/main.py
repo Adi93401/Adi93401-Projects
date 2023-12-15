50c6e08d1fcbf6f7f61366f2d427742f275184ba
Adish Jain <114593154+Adi93401@users.noreply.github.com>
2023-12-15 10:49:23 +0530

    Add files via upload

---
+++ Vchat/main.py
@@ -0,0 +1,44 @@
+from gtts import gTTS  # pip install gtts
+import openai
+import os
+import speech_recognition as sr
+
+openai.api_key = "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"
+
+
+def get_voice_input():
+
+  r = sr.Recognizer()
+
+  with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    print("Listening...")
+    audio_data = r.record(source, duration=5)
+    print("Recognizing...")
+    # convert speech to text
+    text = r.recognize_google(audio_data)
+  return text
+
+
+def get_response(input):
+  response = openai.Completion.create(model="text-davinci-003", prompt=input)
+  return response['choices'][0]['text']
+
+
+def text_to_speech(text, language='en', output_file='output.mp3'):
+  tts = gTTS(text=text, lang=language, slow=False)
+  tts.save(output_file)
+  os.system("start " + output_file)
+
+
+if __name__ == "__main__":
+  print("Chatbot: Hii Adish!! How may I help you?")
+  text_to_speech("Hii Adish!! How may I help you?")
+  while True:
+    user_input = get_voice_input()
+    if user_input == 'exit':
+      break
+    response = get_response(user_input)
+    print(response)
+    text_to_speech(response)
+    text_to_speech("Anything else, Sir")
