f86120105027c814a40789293806562ca2d5bc59
Adish Jain <114593154+Adi93401@users.noreply.github.com>
2024-01-03 12:07:45 +0530

    Update Signup.js

--- I-Notebook/src/components/Signup.js
+++ I-Notebook/src/components/Signup.js
@@ -2,8 +2,27 @@ import React from 'react'
 
 const Signup = () => {
     return (
-        <div>
-            I am signup
+        <div className="container">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">Name</label>
+                <input type="text" className="form-control" id="name" onchange= aria-describedby="emailHelp" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="email" onchange= aria-describedby="emailHelp" />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control" id="password" onchange= />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+                <input type="cpassword" className="form-control" id="cpassword" onchange= />
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     )
 }
