2ff85070cc3d194942f80d88b6ea49e6ab1842d0
Adish Jain <114593154+Adi93401@users.noreply.github.com>
2024-01-03 12:31:25 +0530

    Update Signup.js

--- I-Notebook/src/components/Signup.js
+++ I-Notebook/src/components/Signup.js
@@ -43,11 +43,11 @@ const Signup = () => {
               </div>
               <div className="mb-3">
                 <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" className="form-control" id="password" name="password" onChange={onChange} />
+                <input type="password" className="form-control" id="password" name="password" onChange={onChange} minlength=5 required />
               </div>
               <div className="mb-3">
                 <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-                <input type="cpassword" className="form-control" id="cpassword" name="pcassword" onChange={onChange} />
+                <input type="cpassword" className="form-control" id="cpassword" name="pcassword" onChange={onChange} minlength=5 required />
               </div>
               <button type="submit" className="btn btn-primary">Submit</button>
             </form>
