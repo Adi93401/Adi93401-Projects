a974718e2e2e47db7d46f126d3b53fabdd6eee7c
Adish Jain <114593154+Adi93401@users.noreply.github.com>
2024-01-03 12:44:46 +0530

    Update Alert.js

--- I-Notebook/src/components/Alert.js
+++ I-Notebook/src/components/Alert.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Alert = (props) => {
+export const alert = (props) => {
     const capitalize = (word)=>{
         const lower = word.toLowerCase();
         return lower.charAt(0).toUpperCase() + lower.slice(1);
